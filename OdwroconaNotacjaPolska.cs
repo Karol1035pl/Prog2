e65b744190b4a09403c9b14cd4c63219e2ae7a02
Karol1035pl <Karol-Idzikowski@o2.pl>
2020-04-26 11:44:27 +0200

    dodano nowe funkcje

--- OdwroconaNotacjaPolska.cs
+++ OdwroconaNotacjaPolska.cs
@@ -9,6 +9,7 @@ namespace Prog2
     class OdwroconaNotacjaPolska
     {
         public string Formula { get; set; }
+        public double X { get; set; }
         public OdwroconaNotacjaPolska(string formula)
         {
             formula = formula.ToLower();
@@ -63,6 +64,104 @@ namespace Prog2
             }
             string[] l = k.ToArray();
             return l;
+        }
+        public double ONP_postfix_oblicz(string[] postfix)
+        {
+            double wynik;
+            Stack<string> s = new Stack<string>();
+            for (int i = 0; i < postfix.Length; i++)
+            {
+                if (Regex.IsMatch(postfix[i], @"\d|[x]"))
+                {
+                    if (postfix[i] == "x") s.Push(X.ToString());
+                    else s.Push(postfix[i]);
+                }
+                else if (Regex.IsMatch(postfix[i], @"(pi)|(e)"))
+                {
+                    switch (postfix[i])
+                    {
+                        case "pi":
+                            s.Push(Math.PI.ToString());
+                            break;
+                        case "e":
+                            s.Push(Math.E.ToString());
+                            break;
+                    }
+                }
+                else if (Regex.IsMatch(postfix[i], @"^[-]((abs)|(cos)|(exp)|(log)|(sin)|(sqrt)|(tan)|(cosh)|(sinh)|(tanh)|(acos)|(asin)|(atan))|((abs)|(cos)|(exp)|(log)|(sin)|(sqrt)|(tan)|(cosh)|(sinh)|(tanh)|(acos)|(asin)|(atan))"))
+                {
+                    double temp;
+                    temp = (System.Convert.ToDouble(s.Pop()));
+                    switch (postfix[i])
+                    {
+                        case "abs":
+                            s.Push(Math.Abs(temp).ToString());
+                            break;
+                        case "cos":
+                            s.Push(Math.Cos(temp).ToString());
+                            break;
+                        case "exp":
+                            s.Push(Math.Exp(temp).ToString());
+                            break;
+                        case "log":
+                            s.Push(Math.Log(temp).ToString());
+                            break;
+                        case "sin":
+                            s.Push(Math.Sin(temp).ToString());
+                            break;
+                        case "sqrt":
+                            s.Push(Math.Sqrt(temp).ToString());
+                            break;
+                        case "tan":
+                            s.Push(Math.Tan(temp).ToString());
+                            break;
+                        case "cosh":
+                            s.Push(Math.Cosh(temp).ToString());
+                            break;
+                        case "sinh":
+                            s.Push(Math.Sinh(temp).ToString());
+                            break;
+                        case "tanh":
+                            s.Push(Math.Tanh(temp).ToString());
+                            break;
+                        case "acos":
+                            s.Push(Math.Acos(temp).ToString());
+                            break;
+                        case "asin":
+                            s.Push(Math.Asin(temp).ToString());
+                            break;
+                        case "atan":
+                            s.Push(Math.Atan(temp).ToString());
+                            break;
+                    }
+                }
+                else if (Regex.IsMatch(postfix[i], @"[\-\+\/\*\^]"))
+                {
+                    double a, b;
+                    a = Convert.ToDouble(s.Pop());
+                    b = Convert.ToDouble(s.Pop());
+                    switch (postfix[i])
+                    {
+                        case "+":
+                            s.Push((a + b).ToString());
+                            break;
+                        case "-":
+                            s.Push((b - a).ToString());
+                            break;
+                        case "*":
+                            s.Push((a * b).ToString());
+                            break;
+                        case "/":
+                            s.Push((b / a).ToString());
+                            break;
+                        case "^":
+                            s.Push((Math.Pow(b, a)).ToString());
+                            break;
+                    }
+                }
+            }
+            wynik = Convert.ToDouble(s.Pop());
+            return wynik;
         }
         public int Priorytet(string token)
         {
--- Program.cs
+++ Program.cs
@@ -8,17 +8,21 @@ namespace Prog2
         static void Main(string[] args)
         {
             OdwroconaNotacjaPolska test = new OdwroconaNotacjaPolska("sin(2*x)");
+            test.X = 2;
             string[] tokeny = test.Tokeny();
             for (int i = 0; i < tokeny.Length; i++)
             {
-                System.Console.Write("{0} ", tokeny[i]);
+                    System.Console.Write("{0} ", tokeny[i]);
             }
             string[] postfix = test.ONP_postfix(tokeny);
             System.Console.WriteLine();
             for (int i = 0; i < postfix.Length; i++)
             {
-                System.Console.Write("{0} ", postfix[i]);
+                    System.Console.Write("{0} ", postfix[i]);
             }
+            System.Console.WriteLine();
+            double wynik = test.ONP_postfix_oblicz(postfix);
+            System.Console.WriteLine("{0} ", wynik);
         }
     }
 }
